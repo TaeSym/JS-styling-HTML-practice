49e2391b49fc4916ebf95e7a22daac73bc9d4e03
Shaunté Symone <ShaunteSymone@gmail.com>
2023-06-20 15:14:38 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var instagram = document.querySelector(".fa-instagram");
+instagram.style.color = "#d45bb9";
+
+var facebook = document.querySelector(".fa-facebook");
+facebook.style.color = "#6a99b5";
+
+var hours = document.querySelector(".contact p");
+hours.style.border = "1px solid #d45bb9";
+hours.style.padding = "1em";
